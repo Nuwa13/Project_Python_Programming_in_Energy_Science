e4b71d42e7856f89760b8913ae5e2598f5abeea7
Felix Spraener <felix.spraener@uni-oldenburg.de>
2025-05-28 11:17:46 +0200

    created setup function for foxes windfarm. Use it to simplify the steps needed to create a windfarm with specific properties.

--- customFunctions.py
+++ customFunctions.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import foxes
+import foxes.variables as FV
 
 def read_ts_csv(filename):
     df = pd.read_csv(filename, parse_dates=['Unnamed: 0'])
@@ -13,3 +15,32 @@ def read_ts_csv(filename):
         df[df==-999] = np.nan
         
     return df
+
+def setup_windfarm(wind_data, windfarm_name = 'my_farm', TI = 0.05, RHO = 1.225, wake_models = ["Bastankhah2014_linear_k004"],model_book = None):
+    my_farm = foxes.WindFarm(name=windfarm_name)
+    if model_book is None:
+        my_mbook = foxes.models.ModelBook()
+    else:
+        my_mbook = model_book
+
+    if 'WS' not in wind_data.columns:
+        print('setup_windfarm dailed: wind speed data should be named "WS" but there is no such column in the input data!')
+    if 'WD' not in wind_data.columns:
+        print(
+            'setup_windfarm dailed: wind speed data should be named "WD" but there is no such column in the input data!')
+    my_states = foxes.input.states.Timeseries(
+        data_source=wind_data,
+        output_vars=[FV.WS, FV.WD, FV.TI, FV.RHO],
+        var2col={FV.WS: "WS", FV.WD: "WD"},
+        fixed_vars={FV.RHO: RHO, FV.TI: TI},
+    )
+
+    algo = foxes.algorithms.Downwind(
+        mbook=my_mbook,
+        farm=my_farm,
+        states=my_states,
+        rotor_model="centre",
+        wake_models=wake_models,
+        verbosity=0,
+    )
+    return my_farm
