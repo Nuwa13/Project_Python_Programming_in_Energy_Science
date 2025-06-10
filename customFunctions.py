ca6eb3525e84f69ed3962f562b1b17d041030f32
Jan_Heimsoth <jan@heimsoths.de>
2025-06-10 20:13:21 +0200

    Introduction of the long term correction function. This still needs some minor improvements and also more comments such that usage will be easier.

--- customFunctions.py
+++ customFunctions.py
@@ -2,6 +2,12 @@ import pandas as pd
 import numpy as np
 import foxes
 import foxes.variables as FV
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV, train_test_split, cross_val_predict
+from sklearn.metrics import r2_score, mean_absolute_error, mean_absolute_percentage_error,root_mean_squared_error,mean_squared_error
+
+scaler = StandardScaler()
 
 def read_ts_csv(filename):
     df = pd.read_csv(filename, parse_dates=['Unnamed: 0'])
@@ -99,3 +105,90 @@ def setup_algo(wind_data, windfarm_name = 'my_farm',rotor_model = "centre", TI =
         verbosity=0,
     )
     return my_algo
+
+
+def print_performance(y_predicted,y_true, title=''):
+    print('Validation scores - ' + title + ':')
+    print('-------------------------')
+    print('R2-Score: ',round(r2_score(y_true, y_predicted),4))
+    print('MSE: ',round(mean_squared_error(y_true, y_predicted), 4))
+    print('RMSE: ',round(root_mean_squared_error(y_true, y_predicted), 4))
+    print('MAE: ',round(mean_absolute_error(y_true, y_predicted), 4))
+    print('MAPE: ',round(mean_absolute_percentage_error(y_true, y_predicted)*100, 4))
+    
+
+
+def correct_long_term_wind(wind_model, wind_measurement, classifier, param_grid):
+    # choose a value for comparability
+    RSEED = 42
+
+    # define how many percent of data should be excluded from training to use it for testing
+    test_size = 0.3
+    
+    # set variables 
+    # model data = Features 
+    X = pd.DataFrame(wind_model)
+
+    # valid values = observations
+    Y = pd.DataFrame(wind_measurement)
+
+    # remove nan values
+    # find index of nan values
+    idx = Y[Y.isnull().any(axis=1)].index
+
+    #find index of valid values
+    valid_values = Y[Y.notnull().all(axis=1)].index
+    valid_values = [wind_measurement.index.get_loc(value) for value in valid_values]
+
+    XtoPredict = X.loc[idx]
+
+    # features
+    x = X.iloc[valid_values]
+
+    # target
+    y = Y.iloc[valid_values]
+    xcols = X.columns
+    
+    # split data into training and test data (https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=RSEED)
+
+    # scale data to a common basis (e.g. WS ranges from 0-30 m/s while wind direction from 0 to 360, scaling fits them into a common range)
+    # scaling is done based on trainings data (https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html)
+    X_train_scaled = scaler.fit_transform(X_train)
+
+    # scaling procedure is transfered to test data
+    X_test_scaled = scaler.transform(X_test)
+    
+    # how many times shall the cross validation be done
+    n          = 5
+
+    # according to which metrics shall the performance be tested
+    scoring    = 'neg_mean_squared_error'
+
+    # train the model
+    # GridSearch finds the best parameter combination for the model for which the score is minimal. 
+    # A cross validation is done to compare results from different subsets, 
+    # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html)
+    grid_model = GridSearchCV(classifier, param_grid=param_grid, cv=n, scoring=scoring,\
+                                    verbose=0, n_jobs=-1).fit(X_train_scaled, y_train)
+    
+    best_model = grid_model.best_estimator_
+    print('Best score:\n{:.2f}'.format(grid_model.best_score_))
+    print("Best parameters:\n{}".format(grid_model.best_params_))
+
+    # make predictions using the trainings to check model performance
+    # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_val_predict.html
+    y_train_cv      = cross_val_predict(best_model, X_train_scaled, y_train, cv=n+2)
+
+    # predict values using trained model and test data (features)
+    y_test_predicted = best_model.predict(X_test_scaled)
+
+    # make your final prediction for the entire (long-term) period
+    X_scaledN = scaler.transform(X)
+    
+    print_performance(y_train_cv,y_train, 'Cross Validation')
+    print_performance(x.iloc[:, 0], y.iloc[:, 0], 'Without Correction')
+    print_performance(y_test_predicted, y_test, 'Model and Test Data')
+
+        
+    return best_model.predict(X_scaledN), valid_values
